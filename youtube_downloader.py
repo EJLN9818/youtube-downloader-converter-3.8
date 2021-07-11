a00c3d94b6906a5de8b3c49a405e6f82aae679c4
Emmanuel Lopez <emmanuel.lopez@axiacore.com>
2021-07-11 00:43:45 -0500

    :tada: Added progress bar

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,9 +1,13 @@
 import pytube
+from pytube.cli import on_progress
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def download_video(url, resolution):
     itag = choose_resolution(resolution)
-    video = pytube.YouTube(url)
+    video = pytube.YouTube(url, on_progress_callback=on_progress)
     stream = video.streams.get_by_itag(itag)
+    print(stream.title)
     stream.download()
     return stream.default_filename
 
@@ -16,13 +20,13 @@ def download_playlist(url, resolution):
     download_videos(playlist.video_urls, resolution)
 
 def choose_resolution(resolution):
-    if resolution in ["low", "360", "360p"]:
+    if resolution in ['low', '360', '360p']:
         itag = 18
-    elif resolution in ["medium", "720", "720p", "hd"]:
+    elif resolution in ['medium', '720', '720p', 'hd']:
         itag = 22
-    elif resolution in ["high", "1080", "1080p", "fullhd", "full_hd", "full hd"]:
+    elif resolution in ['high', '1080', '1080p', 'fullhd', 'full_hd', 'full hd']:
         itag = 137
-    elif resolution in ["very high", "2160", "2160p", "4K", "4k"]:
+    elif resolution in ['very high', '2160', '2160p', '4K', '4k']:
         itag = 313
     else:
         itag = 18
@@ -30,12 +34,12 @@ def choose_resolution(resolution):
 
 
 def input_links():
-    print("Enter the links of the videos (end by entering 'STOP'):")
+    print('Enter the links of the videos (end by entering "STOP"):')
 
     links = []
-    link = ""
+    link = ''
 
-    while link != "STOP" and link != "stop":
+    while link not in ['STOP', 'stop']:
         link = input()
         links.append(link)
 
